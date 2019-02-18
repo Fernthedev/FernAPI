c5799fe478474387dc2505cf10f93c4512632ac9
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2019-02-18 12:06:26 -0400

    Switched from Forge to Sponge for simplicity since API is server side API, not meant for client side Sponge is much easier to debug and develop for than Forge

---
+++ fernapi-common/src/main/java/com/github/fernthedev/fernapi/universal/handlers/CommandHandler.java
@@ -0,0 +1,10 @@
+package com.github.fernthedev.fernapi.universal.handlers;
+
+import com.github.fernthedev.fernapi.universal.api.UniversalCommand;
+
+public abstract class CommandHandler {
+
+    public abstract void registerFernCommand(UniversalCommand command);
+
+
+}
