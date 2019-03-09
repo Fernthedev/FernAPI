b2368892021d49ab03ffeb5c98d7061b6526c172
FernPlayzz <15272073+Fernthedev@users.noreply.github.com>
2019-03-09 16:54:35 -0400

    Delete SpongeChatHandler.java

--- src/main/java/com/github/fernthedev/fernapi/server/sponge/chat/SpongeChatHandler.java
+++
@@ -1,6 +0,0 @@
-package com.github.fernthedev.fernapi.server.sponge.chat;
-
-import com.github.fernthedev.fernapi.universal.handlers.IChatHandler;
-
-public class SpongeChatHandler implements IChatHandler {
-}
