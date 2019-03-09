bc60f04ac3da705f3c2ea850e77235a13fbfeb2a
FernPlayzz <15272073+Fernthedev@users.noreply.github.com>
2019-03-09 16:50:05 -0400

    Delete BungeeChatHandler.java

--- src/main/java/com/github/fernthedev/fernapi/server/bungee/chat/BungeeChatHandler.java
+++
@@ -1,6 +0,0 @@
-package com.github.fernthedev.fernapi.server.bungee.chat;
-
-import com.github.fernthedev.fernapi.universal.handlers.IChatHandler;
-
-public class BungeeChatHandler implements IChatHandler {
-}
