9dfc5bb2294d752650fbdd4ed80cbd3a8d02dcd2
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2019-03-11 16:56:51 -0400

    Revert "Delete SpongeChatHandler.java"
    This reverts commit b2368892

---
+++ src/main/java/com/github/fernthedev/fernapi/server/sponge/chat/SpongeChatHandler.java
@@ -0,0 +1,6 @@
+package com.github.fernthedev.fernapi.server.sponge.chat;
+
+import com.github.fernthedev.fernapi.universal.handlers.IChatHandler;
+
+public class SpongeChatHandler implements IChatHandler {
+}
