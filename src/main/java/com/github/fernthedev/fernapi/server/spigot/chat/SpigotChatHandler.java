89801069a776f46cb574f57684047a24623c5956
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2019-03-11 16:58:55 -0400

    Revert "Delete SpigotChatHandler.java"
    This reverts commit 2ab491a2

---
+++ src/main/java/com/github/fernthedev/fernapi/server/spigot/chat/SpigotChatHandler.java
@@ -0,0 +1,6 @@
+package com.github.fernthedev.fernapi.server.spigot.chat;
+
+import com.github.fernthedev.fernapi.universal.handlers.IChatHandler;
+
+public class SpigotChatHandler implements IChatHandler {
+}
