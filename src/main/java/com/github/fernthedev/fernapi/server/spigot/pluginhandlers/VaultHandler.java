befda7112c30b0ace50b2094536ac581e084f8ff
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2019-03-11 16:57:29 -0400

    Revert "Delete VaultHandler.java"
    This reverts commit 1405a797

---
+++ src/main/java/com/github/fernthedev/fernapi/server/spigot/pluginhandlers/VaultHandler.java
@@ -0,0 +1,65 @@
+package com.github.fernthedev.fernapi.server.spigot.pluginhandlers;
+
+import lombok.Getter;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import static org.bukkit.Bukkit.getServer;
+
+
+public class VaultHandler {
+
+    @Getter
+    private static boolean hooked = false;
+
+    public void hook() {
+        if (!setupEconomy() ) {
+            return;
+        }
+        setupPermissions();
+        setupChat();
+        hooked = true;
+    }
+
+    private static Economy econ = null;
+    private static Permission perms = null;
+    private static Chat chat = null;
+
+    public static Economy getEconomy() {
+        return econ;
+    }
+
+    public static Permission getPermissions() {
+        return perms;
+    }
+
+    public static Chat getChat() {
+        return chat;
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
+}
