de4c79f1d2b94a5f69b5c53742933c6d5e893e25
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2019-03-11 16:51:36 -0400

    Revert "Delete DatabaseTest.java"
    This reverts commit b9405fe9

---
+++ src/main/java/com/github/fernthedev/fernapi/universal/examples/DatabaseTest.java
@@ -0,0 +1,61 @@
+package com.github.fernthedev.fernapi.universal.examples;
+
+import com.github.fernthedev.fernapi.universal.DatabaseManager;
+import com.github.fernthedev.fernapi.universal.Universal;
+import com.github.fernthedev.fernapi.universal.data.database.DatabaseInfo;
+import com.github.fernthedev.fernapi.universal.data.database.RowData;
+import com.github.fernthedev.fernapi.universal.data.database.RowObject;
+import com.github.fernthedev.fernapi.universal.data.database.TableInfo;
+
+public class DatabaseTest extends DatabaseManager {
+    private TableInfo tableInfo;
+
+    public DatabaseTest(String username,String password,String port,String URLHost,String database) {
+        connect(new DatabaseInfo(username,password,port,URLHost,database));
+    }
+
+    public TableInfo getTableInfo() {
+        if(tableInfo == null) {
+            tableInfo = new TableInfo("test_no");
+        }
+
+        return tableInfo;
+    }
+
+
+    /**
+     * This is called after you attempt a connection
+     *
+     * @param connected Returns true if successful
+     * @see DatabaseManager#connect(DatabaseInfo)
+     */
+    @Override
+    public void runAfterConnectAttempt(boolean connected) {
+        if(connected) {
+            Universal.getMethods().getLogger().info("Connected successfully");
+        }else{
+            Universal.getMethods().getLogger().warning("Unable to connect successfully");
+        }
+    }
+
+    public void test() {
+
+
+
+        tableInfo = new TableInfo("test_no");
+
+        RowData rowData = new RowData(new RowObject("row1","value1"));
+
+        rowData.addData(new RowObject("row2","value2"));
+
+        tableInfo.addTableInfo(rowData);
+
+        rowData = new RowData(new RowObject("row1","value1nou"));
+
+        rowData.addData(new RowObject("row2","value2nou"));
+
+        tableInfo.addTableInfo(rowData);
+
+        createTable(tableInfo);
+    }
+}
