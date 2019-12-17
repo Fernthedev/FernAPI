c5c0a794ed117ba6fec3a698e33f511c2f89f3c5
Fern <15272073+Fernthedev@users.noreply.github.com>
2019-12-17 14:29:43 -0400

    Moved to a multi-module structure to allow using specific server platforms and to encapsulate dependencies of server platforms to reduce NoClassDef exceptions

---
+++ core/src/main/java/com/github/fernthedev/fernapi/universal/handlers/UUIDFetchManager.java
@@ -0,0 +1,75 @@
+package com.github.fernthedev.fernapi.universal.handlers;
+
+import com.github.fernthedev.fernapi.universal.Universal;
+import com.github.fernthedev.fernapi.universal.data.ScheduleTaskWrapper;
+import com.github.fernthedev.fernapi.universal.util.UUIDFetcher;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.github.fernthedev.fernapi.universal.util.UUIDFetcher.*;
+
+public class UUIDFetchManager {
+//    void stopTimerRequest();
+//
+//    void stopHourTask();
+//
+//    void runTimerRequest();
+//
+//    void runHourTask();
+//
+//    String getNameFromPlayer(UUID uuid);
+//
+//    UUID getUUIDFromPlayer(String name);
+
+    private static ScheduleTaskWrapper requestTask;
+
+    private static ScheduleTaskWrapper banHourTask;
+
+    public void runTimerRequest() {
+        debug("Server is bungee");
+        requestTask = Universal.getScheduler().runSchedule(() -> {
+            UUIDFetcher.setRequests(0);
+            playerNameCache.clear();
+            playerUUIDCache.clear();
+            playerHistoryCache.clear();
+            debug("Refreshed uuid cache.");
+        }, 1, 10, TimeUnit.MINUTES);
+    }
+
+    public void runHourTask() {
+
+        banHourTask = Universal.getScheduler().runSchedule( () -> {
+            if (!hourRan && didHourCheck) {
+                hourRan = true;
+                addRequestTimer();
+                stopHourTask();
+            } else if (!didHourCheck) didHourCheck = true;
+        }, 1, 1, TimeUnit.HOURS);
+
+    }
+
+
+
+
+    public void stopTimerRequest() {
+        if(requestTask != null) {
+            requestTask.cancel();
+        }
+    }
+
+    public void stopHourTask() {
+        if(banHourTask != null)
+            banHourTask.cancel();
+
+    }
+
+
+
+    private static void print(Object log) {
+        Universal.getMethods().getLogger().info("[" + Universal.getMethods().getServerType() + "] [UUIDFetcher] " + log);
+    }
+
+    private static void debug(Object log) {
+        Universal.debug("[" + Universal.getMethods().getServerType() + "] [UUIDFetcher] " + log);
+    }
+}
---
+++ spigot/src/main/java/com/github/fernthedev/fernapi/server/spigot/SpigotInterface.java
@@ -0,0 +1,101 @@
+package com.github.fernthedev.fernapi.server.spigot;
+
+import com.github.fernthedev.fernapi.server.spigot.player.SpigotFConsole;
+import com.github.fernthedev.fernapi.server.spigot.player.SpigotFPlayer;
+import com.github.fernthedev.fernapi.universal.Universal;
+import com.github.fernthedev.fernapi.universal.api.CommandSender;
+import com.github.fernthedev.fernapi.universal.api.IFPlayer;
+import com.github.fernthedev.fernapi.universal.handlers.FernAPIPlugin;
+import com.github.fernthedev.fernapi.universal.handlers.MethodInterface;
+import com.github.fernthedev.fernapi.universal.handlers.ServerType;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class SpigotInterface implements MethodInterface {
+    private FernSpigotAPI fernSpigotAPI;
+    SpigotInterface(FernSpigotAPI fernSpigotAPI) {
+        this.fernSpigotAPI = fernSpigotAPI;
+    }
+
+    @Override
+    public Logger getLogger() {
+        return fernSpigotAPI.getLogger();
+    }
+
+    @Override
+    public ServerType getServerType() {
+        return ServerType.BUKKIT;
+    }
+
+    @Override
+    public FernAPIPlugin getInstance() {
+        return fernSpigotAPI;
+    }
+
+    @Override
+    public IFPlayer convertPlayerObjectToFPlayer(Object player) {
+        return new SpigotFPlayer((Player) player);
+    }
+
+    @Override
+    public Player convertFPlayerToPlayer(IFPlayer ifPlayer) {
+        if(ifPlayer instanceof SpigotFPlayer) {
+            return ((SpigotFPlayer) ifPlayer).getPlayer();
+        }
+
+        return Bukkit.getPlayer(ifPlayer.getUuid());
+
+    }
+
+    @Override
+    public CommandSender convertCommandSenderToAPISender(Object commandSender) {
+        if(commandSender instanceof Player) {
+            return new SpigotFPlayer((Player) commandSender);
+        }
+
+        if(commandSender instanceof org.bukkit.command.CommandSender) {
+            return new SpigotFConsole((org.bukkit.command.CommandSender) commandSender);
+        }
+
+        return null;
+    }
+
+    @Override
+    public IFPlayer getPlayerFromName(String name) {
+        return convertPlayerObjectToFPlayer(fernSpigotAPI.getServer().getPlayer(name));
+    }
+
+    @Override
+    public IFPlayer getPlayerFromUUID(UUID uuid) {
+        return convertPlayerObjectToFPlayer(fernSpigotAPI.getServer().getPlayer(uuid));
+    }
+
+    @Override
+    public List<IFPlayer> getPlayers() {
+        return Bukkit.getOnlinePlayers().stream().map(proxiedPlayer -> Universal.getMethods().convertPlayerObjectToFPlayer(proxiedPlayer)).collect(Collectors.toList());
+    }
+
+    @Override
+    public File getDataFolder() {
+        return fernSpigotAPI.getDataFolder();
+    }
+
+    @Override
+    public String getNameFromPlayer(UUID uuid) {
+        if (fernSpigotAPI.getServer().getPlayer(uuid) == null) return null;
+        return Objects.requireNonNull(fernSpigotAPI.getServer().getPlayer(uuid)).getName();
+    }
+
+    @Override
+    public UUID getUUIDFromPlayer(String name) {
+        if (fernSpigotAPI.getServer().getPlayer(name) == null) return null;
+        return Objects.requireNonNull(fernSpigotAPI.getServer().getPlayer(name)).getUniqueId();
+    }
+}
